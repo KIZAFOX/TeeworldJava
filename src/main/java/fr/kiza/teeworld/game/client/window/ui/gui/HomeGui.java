d5f04fd262e3f2802519c27875a58b2b3db23e54
KIZA <victor.pernak@hotmail.fr>
2024-10-24 10:42:02 +0200

    :cyclone: adding button clicks/render/update

--- src/main/java/fr/kiza/teeworld/game/client/window/ui/gui/HomeGui.java
+++ src/main/java/fr/kiza/teeworld/game/client/window/ui/gui/HomeGui.java
@@ -1,13 +1,16 @@
 package fr.kiza.teeworld.game.client.window.ui.gui;
 
-import fr.kiza.teeworld.game.client.status.GameState;
 import fr.kiza.teeworld.game.client.window.Game;
-import fr.kiza.teeworld.game.client.window.ui.handler.ActionListener;
-import fr.kiza.teeworld.game.client.window.ui.handler.GuiHandler;
+import fr.kiza.teeworld.game.client.window.ui.button.handler.ButtonAction;
+import fr.kiza.teeworld.game.client.window.ui.button.handler.ButtonHandler;
+import fr.kiza.teeworld.game.client.window.ui.gui.handler.ActionListener;
+import fr.kiza.teeworld.game.client.window.ui.gui.handler.GuiHandler;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+
+import java.util.function.BiConsumer;
 
 import static fr.kiza.teeworld.game.client.window.GamePanel.*;
 
@@ -22,11 +25,13 @@ public class HomeGui extends GuiHandler implements ActionListener {
         graphics.setColor(Color.BLACK);
 
         this.renderGrill(graphics);
+
+        this.renderButtons(graphics);
     }
 
     @Override
     public void update() {
-
+        this.updateButtons();
     }
 
     @Override
@@ -36,17 +41,16 @@ public class HomeGui extends GuiHandler implements ActionListener {
 
     @Override
     public void mousePressed(MouseEvent event) {
-
+        this.handleButtonEvent(event, ButtonAction::mousePressed);
     }
 
     @Override
     public void mouseReleased(MouseEvent event) {
-
+        this.handleButtonEvent(event, ButtonAction::mouseReleased);
     }
 
     @Override
     public void mouseEntered(MouseEvent event) {
-
     }
 
     @Override
@@ -61,7 +65,7 @@ public class HomeGui extends GuiHandler implements ActionListener {
 
     @Override
     public void mouseMoved(MouseEvent event) {
-
+        this.handleButtonEvent(event, ButtonAction::mouseMoved);
     }
 
     @Override
@@ -71,13 +75,27 @@ public class HomeGui extends GuiHandler implements ActionListener {
 
     @Override
     public void keyPressed(KeyEvent event) {
-        if(event.getKeyCode() == KeyEvent.VK_UP){
-            GameState.setCurrentState(GameState.PLAY);
-        }
+
     }
 
     @Override
     public void keyReleased(KeyEvent event) {
 
+    }
+
+    private void renderButtons(final Graphics2D graphics) {
+        this.game.getButtonBuilder().getButton().forEach(buttons -> buttons.render(graphics));
+    }
+
+    private void updateButtons(){
+        this.game.getButtonBuilder().getButton().forEach(ButtonHandler::update);
+    }
+
+    private void handleButtonEvent(final MouseEvent event, final BiConsumer<ButtonAction, MouseEvent> action){
+        this.game.getButtonBuilder().getButton().forEach(buttons -> {
+            if(buttons.getBounds().contains(event.getPoint())){
+                action.accept((ButtonAction) buttons, event);
+            }
+        });
     }
 }
