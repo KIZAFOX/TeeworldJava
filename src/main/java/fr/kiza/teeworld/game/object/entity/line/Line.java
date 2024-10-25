2b3b45d313dbee921ee102ef80105dc535624d2d
KIZA <victor.pernak@hotmail.fr>
2024-10-25 16:56:20 +0200

    :cyclone: line OK

--- src/main/java/fr/kiza/teeworld/game/object/entity/line/Line.java
+++ src/main/java/fr/kiza/teeworld/game/object/entity/line/Line.java
@@ -9,19 +9,24 @@ import java.awt.*;
 public class Line extends GameObject {
 
     private Point start, end;
+    private double angle;
 
     private final int fixedLength;
 
     public Line(Game game, Point start, int length) {
-        super(game, ObjectType.LINE, 0, 0);
+        super(game, ObjectType.LINE, 100, 100);
         this.start = start;
-        this.end = new Point(start.x + length, start.y);
+        this.end = new Point(start.x, start.y);
         this.fixedLength = length;
+        this.angle = 0;
     }
+
     @Override
-    public void render(final Graphics2D graphics){
+    public void render(final Graphics2D graphics) {
+        final int endX = (int) (start.x + fixedLength * Math.cos(angle)), endY = (int) (start.y + fixedLength * Math.sin(angle));
+
         graphics.setColor(Color.RED);
-        graphics.drawLine(start.x, start.y, end.x, end.y);  // C'est correct
+        graphics.drawLine(start.x, start.y, endX, endY);
     }
 
     @Override
@@ -34,23 +39,9 @@ public class Line extends GameObject {
         return new Rectangle((int) this.x, (int) this.y, this.width, this.height);
     }
 
-    public void updateEnd(final Point mousePosition, final Point playerPosition) {
-        this.start = playerPosition;
-
-        // Calculez l'angle entre le point de départ et la position de la souris
-        double angle = Math.atan2(mousePosition.y - start.y, mousePosition.x - start.x);
-
-        // Calculez la distance réelle entre le joueur et la souris
-        double distance = start.distance(mousePosition);
-
-        double lengthToUse = Math.min(fixedLength, distance);
-
-        int newX = (int) (start.x + lengthToUse * Math.cos(angle));
-        int newY = (int) (start.y + lengthToUse * Math.sin(angle));
-
-        this.end = new Point(newX, newY);
-
-        System.out.println("New End (Using Length): " + this.end);
+    public void updateDirection(final Point mousePosition) {
+        final double dx = mousePosition.x - start.x, dy = mousePosition.y - start.y;
+        this.angle = Math.atan2(dy, dx);
     }
 
     public Point getStart() {
