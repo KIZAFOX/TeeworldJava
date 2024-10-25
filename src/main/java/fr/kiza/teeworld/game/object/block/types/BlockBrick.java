bfa1784edbe5e6c5bdce565b55903e251ebcea12
KIZA <victor.pernak@hotmail.fr>
2024-10-25 16:50:33 +0200

    :truck: file moved/rename

--- src/main/java/fr/kiza/teeworld/game/object/block/types/BlockBrick.java
+++ src/main/java/fr/kiza/teeworld/game/object/block/types/BlockBrick.java
@@ -1,28 +1,32 @@
-package fr.kiza.teeworld.game.object.block;
+package fr.kiza.teeworld.game.object.block.types;
 
 import fr.kiza.teeworld.game.client.window.Game;
 import fr.kiza.teeworld.game.map.MapUtil;
+import fr.kiza.teeworld.game.map.style.MapStyle;
 import fr.kiza.teeworld.game.object.*;
+import fr.kiza.teeworld.game.object.block.handler.Block;
 
 import java.awt.*;
 
 import static fr.kiza.teeworld.game.client.window.GamePanel.*;
 
-public class Block extends GameObject {
-    public Block(Game game, float x, float y) {
+public class BlockBrick extends Block {
+    public BlockBrick(Game game, float x, float y) {
         super(game, ObjectType.BLOCK, x, y);
         this.width = PIXEL;
         this.height = PIXEL;
+
+        this.setImage(MapUtil.BRICK);
     }
 
     @Override
     public void render(Graphics2D graphics) {
-        MapUtil.render(graphics, MapUtil.BRICK, (int) x, (int) y);
+        MapUtil.render(graphics, this.getImage(), (int) x, (int) y);
     }
 
     @Override
     public void update() {
-
+        this.setImage(MapStyle.getBrickImage());
     }
 
     @Override
